6a674ebf091ace7e660c201edbbea811d8de39a0
meikku <59999917+meikku@users.noreply.github.com>
2020-11-03 10:36:57 +0200

    ex 3.3 done

--- index.js
+++ index.js
@@ -36,7 +36,18 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/api/info', (request, response) => {
   response.send(`Phonebook has info for ${persons.length} people </br>  ${date}`)
- 
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end() 
+  }
 })
 
 
