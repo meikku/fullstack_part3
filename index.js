31f024f3043a947f1e16a595710f74751fe38688
meikku <59999917+meikku@users.noreply.github.com>
2020-11-09 10:20:06 +0200

    ex 3.9 done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require ('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.json())
 
