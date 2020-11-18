2a352a47562f48b49f230bae0b0b02cb21c972a0
meikku <59999917+meikku@users.noreply.github.com>
2020-11-18 09:06:13 +0200

    uibuild

--- mongo.js
+++ mongo.js
@@ -7,9 +7,8 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url =
-    `mongodb+srv://fullstack:${password}@cluster0.rvpqg.mongodb.net/phonebook?retryWrites=true&w=majority`
-
+const url = process.env.MONGODB_URI
+    
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
