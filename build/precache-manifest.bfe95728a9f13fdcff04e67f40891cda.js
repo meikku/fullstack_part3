1dc06e0974b9f31d657a12167ba657b11d9b183c
meikku <59999917+meikku@users.noreply.github.com>
2020-11-20 11:44:48 +0200

    uibuild

---
+++ build/precache-manifest.bfe95728a9f13fdcff04e67f40891cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4baa516c3ea6b90e9f99a4fbc78cde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3ea03ab52e46f64ed81",
+    "url": "/static/css/main.93253db5.chunk.css"
+  },
+  {
+    "revision": "6fa96f4e77e54634d75a",
+    "url": "/static/js/2.89f8abb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89f8abb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ea03ab52e46f64ed81",
+    "url": "/static/js/main.919976e0.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
