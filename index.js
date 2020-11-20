094fcdd47b522bdabf8ef9ce592e1d7613f03712
meikku <59999917+meikku@users.noreply.github.com>
2020-11-20 09:55:57 +0200

    fix ,

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const app = express()
 require('dotenv').config()
@@ -15,34 +14,33 @@ app.use(express.json())
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
-    response.json(persons.map(person =>person.toJSON()))
+    response.json(persons.map(person => person.toJSON()))
   })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
-  .then(person => {
-    if (person) {
-      response.json(person)
-    }
-    else {
-      response.status(404).end() 
-    }
-  })
-  .catch(error => next(error))
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-  .then(result => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  
+
   if (!body.name) {
     return response.status(400).json({
       error: 'name missing'
@@ -53,33 +51,37 @@ app.post('/api/persons', (request, response) => {
       error: 'number missing'
     })
   }
-  
+
   if (persons.some(person => person.name === body.name)) {
     return response.status(400).json({
-      error: 'name must be unique' 
+      error: 'name must be unique'
     })
   }
 
-  
+
   const person = new Person({
     name: body.name,
     number: body.number,
   })
-person.save().then(savedPerson => {
-  response.json(savedPerson)
-})
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({
+    error: 'unknown endpoint'
+  })
 }
 app.use(unknownEndpoint)
 
-const errorHandler = (error, request, response, next) => 
-{  console.error(error.message)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
   }
   next(error)
 }
