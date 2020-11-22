6af2dd80b2b6eb0715e932eff67513e25febbf9c
Merja Pyrhönen SOFTK20P <merja.pyrhonen@centria.fi>
2020-11-22 11:36:58 +0200

    uibuild

---
+++ build/precache-manifest.90ca3b9ec06fca801a4f31e5f4fae3f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f467c402169f7973646e5bd67945646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6838ec796926bde55d81",
+    "url": "/static/css/main.198f1822.chunk.css"
+  },
+  {
+    "revision": "6fa96f4e77e54634d75a",
+    "url": "/static/js/2.89f8abb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89f8abb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6838ec796926bde55d81",
+    "url": "/static/js/main.58aec27e.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
